93c9744c66b9180000ecada2683c33fb01d7da03
mateusmaso <m.maso25@gmail.com>
2013-07-29 01:27:36 -0300

    whitespace fix

--- lib/ractive/rails/engine.rb
+++ lib/ractive/rails/engine.rb
@@ -1,11 +1,11 @@
 module Ractive
   module Rails
-	  class Engine < ::Rails::Engine
-	  	config.before_initialize do |app|
-	  		Sprockets::Engines
-	  		Sprockets.register_engine('.rac', Tilt)
-	  		Sprockets.register_engine('.ractive', Tilt)
-	  	end
-	  end
-	end
+    class Engine < ::Rails::Engine
+      config.before_initialize do |app|
+        Sprockets::Engines
+        Sprockets.register_engine('.rac', Tilt)
+        Sprockets.register_engine('.ractive', Tilt)
+      end
+    end
+  end
 end
